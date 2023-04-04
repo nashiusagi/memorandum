c24b4c4576a02bdef6aa6d9594199677d994698f
nashiusagi <ritotech0226@gmail.com>
2023-04-05 07:20:04 +0900

    wip

--- app/resources/js/Pages/Tip/Create.jsx
+++ app/resources/js/Pages/Tip/Create.jsx
@@ -1,12 +1,12 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "@inertiajs/react";
 //import ColorSchemeToggle from "../../Components/ColorSchemeToggle";
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from "react-markdown";
 
 const previewStyle = {
     display: "flex",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
 };
 
 const Create = () => {
@@ -17,7 +17,7 @@ const Create = () => {
 
     const [markedBody, setMarkedBody] = useState("");
 
-    const handleChange = (e) => {
+    const handleChange = async (e) => {
         const key = e.target.id;
         const value = e.target.value;
 
@@ -26,7 +26,7 @@ const Create = () => {
             [key]: value,
         }));
 
-        axios
+        await axios
             .post(route("tips.markdown"), values)
             .then((response) => {
                 console.log(response.data);
@@ -51,6 +51,28 @@ const Create = () => {
             });
     };
 
+    useEffect(() => {
+        // 入力欄にカーソルを合わせたときに、何も入力されていない状態でサーバに渡されるのを防ぐ
+        if (values.body.trim().length != 0) {
+            // 入力し終わってから2秒後に自動保存
+            const timer = setTimeout(async () => {
+                await axios
+                    .post(route("tips.markdown"), values)
+                    .then((response) => {
+                        window.alert("保存しました");
+                        console.log(response.data);
+
+                        setMarkedBody(response.data);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }, 2000);
+
+            return () => clearTimeout(timer);
+        }
+    });
+
     return (
         <>
             タイトル：
@@ -65,7 +87,7 @@ const Create = () => {
                 <br />
                 本文：
                 <div style={previewStyle}>
-                    <div style={{width:"50%", marginRight: 10}}>
+                    <div style={{ width: "50%", marginRight: 10 }}>
                         <textarea
                             id="body"
                             name="tips[body]"
@@ -73,8 +95,9 @@ const Create = () => {
                             onChange={handleChange}
                         />
                     </div>
-                    <div style={{width:"50%"}}>
-                        Preview<br />
+                    <div style={{ width: "50%" }}>
+                        Preview
+                        <br />
                         <ReactMarkdown children={markedBody} />
                     </div>
                 </div>
